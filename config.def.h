f34ed6bc659975d8ce952be9693db1960c6d4c69
Axel Moinet (bullekeup) <bullekeup@bullekeup.net>
2020-11-02 12:46:00 +0100

    Modified default config with azerty keybindings and window rules

--- config.def.h
+++ config.def.h
@@ -8,20 +8,21 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh_percent    = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh_percent * screen_height / 100 as bar height */
-static const int user_bh            = 35;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
-static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
+static const int user_bh            = 50;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_lrpad         = 30;
+static const int vertpad            = 0;       /* vertical padding of bar */
+static const int sidepad            = 0;       /* horizontal padding of bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
 static const char *altbarcmd        = "$HOME/.config/polybar/launch.sh"; /* Alternate bar launch command */
-static const char *fonts[]          = { "Iosevka Nerd Font:bold:size=14" };
-static const char dmenufont[]       = "Iosevka Nerd Font:bold:size=14";
+static const char *fonts[]          = { "MesloLGS NF:bold:size=18" };
+static const char dmenufont[]       = "MesloLGS NF:bold:size=18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#bbbbbb";
+static const char col_cyan[]        = "#2865AC";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -29,7 +30,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "﬙", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -37,12 +38,27 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class        instance    title            tags mask     iscentered   isfloating  isterminal  noswallow    monitor    scratch key  isfixedontop*/
-	{ "Gimp",       NULL,       NULL,            0,            0,           1,          0,          0,           -1,        0,           0   },
-	{ "Firefox",    NULL,       NULL,            1 << 8,       0,           0,          0,          1,           -1,        0,           0   },
+	{ "Gimp",       NULL,       NULL,            1 << 8,       0,           1,          0,          0,           -1,        0,           0   },
+	{ "Inkscape",   NULL,       NULL,            1 << 8,       0,           1,          0,          0,           -1,        0,           0   },
+	{ "Firefox",    NULL,       NULL,            1 << 1,       0,           0,          0,          1,           -1,        0,           0   },
 	{ "Alacritty",  NULL,       NULL,            0,            0,           0,          1,          0,           -1,        0,           0   },
 	{ NULL,         NULL,      "scratchpad",     0,            1,           1,          0,          1,           -1,       's',          0   },
 	{ NULL,         NULL,      "Event Tester",   0,            0,           0,          0,          1,           -1,        0,           0   }, /* xev */
-	{ "Polybar",    NULL,      NULL,             0,            0,           0,          0,          1,           -1,        0,           1   },
+	{ "element-nightly", NULL, NULL,             1 << 2,       0,           0,          0,          0,           -1,        0,           0   },
+	{ "Mail",      NULL,       NULL,             1 << 2,       0,           0,          0,          0,           -1,        0,           0   },
+	{ "Thunderbird", NULL,     NULL,             1 << 2,       0,           0,          0,          0,           -1,        0,           0   },
+	{ NULL,        NULL,       "MuttTerm",       1 << 2,       0,           0,          0,          0,           -1,        0,           0   },
+	{ NULL,        NULL,       "KhalTerm",       1 << 2,       0,           0,          0,          0,           -1,        0,           0   },
+	{ NULL,        NULL,       "KhardTerm",      1 << 2,       0,           0,          0,          0,           -1,        0,           0   },
+	{ "brave-browser", NULL,   NULL,             1 << 1,       0,           0,          0,          0,           -1,        0,           0   },
+	{ "Zathura",   NULL,       NULL,             1 << 3,       0,           0,          0,          0,           -1,        0,           0   },
+	{ "okular",    NULL,       NULL,             1 << 3,       0,           0,          0,          0,           -1,        0,           0   },
+	{ NULL,        NULL,       "VimTerm",        1 << 4,       0,           0,          0,          0,           -1,        0,           0   },
+	{ "DesktopEditors", NULL,  NULL,             1 << 4,       0,           0,          0,          0,           -1,        0,           0   },
+	{ "code-oss",  NULL,       NULL,             1 << 5,       0,           0,          0,          0,           -1,        0,           0   },
+	{ NULL,        NULL,       "GDBTerm",        1 << 5,       0,           0,          0,          0,           -1,        0,           0   },
+	{ NULL,        NULL,       "SSHTerm",        1 << 6,       0,           0,          0,          0,           -1,        0,           0   },
+	{ "kicad",     NULL,       NULL,             1 << 7,       0,           0,          0,          0,           -1,        0,           0   },
 };
 
 /* layout(s) */
@@ -78,6 +94,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *launchercmd[] = { "sh", "-c", "~/.local/share/dwm/launcher" };
 
 /*First arg only serves to match against key in rules*/
 static const char *scratchpadcmd[] = {"s", "alacritty", "-t", "scratchpad", NULL};
@@ -87,10 +104,10 @@ static const char *togglepolybarcmd[] = {"polybar-msg", "cmd", "toggle", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = launchercmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      spawn,          {.v = togglepolybarcmd } },
+	{ MODKEY,                       XK_n,      togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_b,      togglefixedontop, {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -112,25 +129,25 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_s,      togglesticky,   {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       0xe0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             0xe0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
+	{ MODKEY,                       XK_plus,   setgaps,        {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        0x26,                      0)
+	TAGKEYS(                        0xe9,                      1)
+	TAGKEYS(                        0x22,                      2)
+	TAGKEYS(                        0x27,                      3)
+	TAGKEYS(                        0x28,                      4)
+	TAGKEYS(                        0x2d,                      5)
+	TAGKEYS(                        0xe8,                      6)
+	TAGKEYS(                        0x5f,                      7)
+	TAGKEYS(                        0xe7,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
